118c5d439435f54ee50e7863c06fed66901cd763
= <luismoura2005@gmail.com>
2022-11-11 16:44:58 -0300

    corrigido bug das fotos dos peixes e adicionado animação na navbar

---
+++ js/aquarios.js
@@ -0,0 +1,7 @@
+
+function fecharModal(){
+    let modal = document.querySelector('.modal')
+    let fundoModal = document.querySelector('.fundo-modal')
+    modal.style.display = 'none'
+    fundoModal.style.display = 'none'
+}
